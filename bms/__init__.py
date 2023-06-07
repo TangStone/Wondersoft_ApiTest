af370abbd4f7d02dfef5b98ce43c7f5c11c3a893
郜洁 <gaojie@wondersoft.cn>
2023-06-07 19:19:36 +0800

    参数替换方式优化

--- bms/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@author: gaojie
-@File: __init__.py.py
-@IDE: PyCharm
-@time: 2023-06-04 18:47
-@description:
-"""
--- common/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@author: gaojie
-@File: __init__.py.py
-@IDE: PyCharm
-@time: 2023-06-02 15:56
-@description:
-"""
